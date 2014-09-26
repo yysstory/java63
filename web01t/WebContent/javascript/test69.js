39b5e41187f1bfd23780073965b2c35a6c93b883
jinyoung <jinyoung.eom@gmail.com>
2014-09-26 14:57:49 +0900

    .

--- web01t/WebContent/javascript/test69.js
+++ web01t/WebContent/javascript/test69.js
@@ -1,33 +1,33 @@
 "use strict"; 
 
-// 목표: 라이브러리화? 
-// 자주 사용할 함수는 별도의 파일에 분리하여 둔다.
+//목표: 라이브러리화2? 
+//기존 엘리먼트 객체에 나만의 조리료를 뿌리자!
 
 changeState('create');
 
 var toYYYYMMDD = new Date();
 
 function changeState(state) {
-  var stateMap = {
-    create: 'none',
-    detail: 'none'
-  };
-  
-  stateMap[state] = '';
-  
-  var detailClass = document.querySelectorAll('.detail');
-  var createClass = document.querySelectorAll('.create');
-  
-  for (var i = 0; i < detailClass.length; i++) {
-    detailClass[i].style.display = stateMap.detail;
-  }
-  
-  for (var i = 0; i < createClass.length; i++) {
-    createClass[i].style.display = stateMap.create;
-  }
+	var stateMap = {
+			create: 'none',
+			detail: 'none'
+	};
+
+	stateMap[state] = '';
+
+	var detailClass = document.querySelectorAll('.detail');
+	var createClass = document.querySelectorAll('.create');
+
+	for (var i = 0; i < detailClass.length; i++) {
+		detailClass[i].style.display = stateMap.detail;
+	}
+
+	for (var i = 0; i < createClass.length; i++) {
+		createClass[i].style.display = stateMap.create;
+	}
 }
 
-// 게시글을 저장하는 객체 생성자 함수
+//게시글을 저장하는 객체 생성자 함수
 function Board(title, content, writer, password) {
 	this.title = title;
 	this.content = content;
@@ -41,19 +41,19 @@ function resetForm() {
 	// reset 버튼에게 click 이벤트를 전달 
 	//1) MouseEvent 객체 생성
 	var event = new MouseEvent('click', {
-    'view': window,
-    'bubbles': true,
-    'cancelable': true
-  });
-	
-  //2) reset  버튼에게 이벤트 전달
-  $('#btnCancel').dispatchEvent(event);
+		'view': window,
+		'bubbles': true,
+		'cancelable': true
+	});
+
+	//2) reset  버튼에게 이벤트 전달
+	$('#btnCancel').dispatchEvent(event);
 }
 
 var boardList = [];
 
 $('#btnCancel').onclick = function(event) {
-  changeState('create');
+	changeState('create');
 }
 
 $('#btnAdd').onclick = function(event) {
@@ -62,11 +62,11 @@ $('#btnAdd').onclick = function(event) {
 			$('#content').value,
 			$('#writer').value,
 			$('#password').value);
-	
+
 	boardList.push(board);
-	
+
 	resetForm();
-	
+
 	refreshBoardList();
 };
 
@@ -78,55 +78,47 @@ function refreshBoardList() {
 		//console.log(tbody.children[i]);
 		tbody.removeChild(tbody.children[i]);
 	}
-	  
+
 	var tr = null;
 	var td = null;
 	var a = null;
-	var text = null;
 	for (var i in boardList) {
 		tr = $('<tr>');
-		
+
 		td = $('<td>');
-		text = document.createTextNode(i);
-		td.appendChild(text);
+		td.html(i);
 		tr.appendChild(td);
-    
-		td = $('<td>');//td 생성   
-		a = $('<a>');//a 생성
-		// 일반 객체가 아니라 엘리먼트에 없던 속성을 추가할 때는
-		// setAttribute()를 사용한다.
+
+		td = $('<td>');   
+		a = $('<a>');
 		a.setAttribute('bno', new String(i));
 		a.href = '#';
 		a.onclick = loadBoardDetail;
-		text = document.createTextNode(boardList[i].title); //제목 생성
-		a.appendChild(text);//a에 붙인다.
-	  td.appendChild(a);//a를 td에 붙인다.
-	  tr.appendChild(td);
-		
-	  td = $('<td>');
-    text = document.createTextNode(boardList[i].writer);
-    td.appendChild(text);
-	  tr.appendChild(td);
-	  
-	  td = $('<td>');
-	  text = document.createTextNode($.toYYYYMMDD(boardList[i].date));
-	  td.appendChild(text);
-	  tr.appendChild(td);
-	  
-	  td = $('<td>');
-	  text = document.createTextNode(boardList[i].count);
-	  td.appendChild(text);
-	  tr.appendChild(td);
-	  
-	  tbody.appendChild(tr);
+		a.html(boardList[i].title); 
+		td.appendChild(a);
+		tr.appendChild(td);
+
+		td = $('<td>');
+		td.html(boardList[i].writer);
+		tr.appendChild(td);
+
+		td = $('<td>');
+		td.html($.toYYYYMMDD(boardList[i].date));
+		tr.appendChild(td);
+
+		td = $('<td>');
+		td.html(boardList[i].count);
+		tr.appendChild(td);
+
+		tbody.appendChild(tr);
 	}
 }
 
 function loadBoardDetail(event) {
 	event.preventDefault();
-	
+
 	changeState('detail');
-	
+
 	var board = boardList[this.getAttribute('bno')];
 	$('#no').value = this.getAttribute('bno');
 	$('#title').value = board.title;
--- web01t/WebContent/javascript/test69_common.js
+++ web01t/WebContent/javascript/test69_common.js
@@ -1,11 +1,23 @@
 "use strict"; 
 
+//window.bit = function(value) {
 function bit(value) {
+	var element = null;
 	if (value.charAt(0) == '#') { // 아이디일 경우,
-	  return document.getElementById(value.substring(1));
+	  element = document.getElementById(value.substring(1));
 	} else if (value.charAt(0) == '<') { // 태그일 경우,
-	  return document.createElement(value.replace(/<|>/g, ''));	
+	  element = document.createElement(value.replace(/<|>/g, ''));	
 	}
+	
+	element.text = function(value) {
+		this.textContent = value;
+	};
+	
+	element.html = function(value) {
+		this.innerHTML = value;
+	};
+	
+	return element;
 }
 
 var $ = bit;
