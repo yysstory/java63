e5714d4890596f3ccccf55ffd1bcc54bd39ba4e9
jinyoung <jinyoung.eom@gmail.com>
2014-10-10 11:40:59 +0900

    .

--- node01/test07.js
+++ node01/test07.js
@@ -1,16 +1,25 @@
 /*
-HTML 출력하기
+요청 파라미터의 값 꺼내기
+- 클라이언트가 보낸 데이터(요청 파라미터)
+- 
  */
 var http = require('http');
+//1. URL 모듈을 가져온다
+var url = require('url');
 
 http.createServer(function handler(req, res) {
+	// 클라이언트가 보낸 name 파라미터 값을 변수에 저장하라!
+	//2. URL 정보를 분석한다.
+	var urlMap = url.parse(req.url, true);
+	
+	//3. 쿼리 객체에서 파라미터 값을 추출한다.
+	var name = urlMap.query.name;
+	
     res.writeHead(200, 'ok', 
     		{'Content-Type': 'text/html;charset=UTF-8'});
     res.write('<html><head><title>test04</title></head>');
     res.write('<body>');
-    res.write('<h1>오호라... 이것이 노드제이에스</h1>')
-    res.write('<p>자바스크립트만 제대로 배워도 서버측 ' +
-    		'애플리케이션을 개발할 수 있다.</p>');
+    res.write('<h1>안녕하세요. ' + name + '님!</h1>')
     res.write('</body></html>');
     res.end();
 	console.log('okoko');
---
+++ node01/test08.js
@@ -0,0 +1,89 @@
+/*
+요청 파라미터의 값 꺼내기
+- 클라이언트가 보낸 데이터(요청 파라미터)
+- 
+ */
+var http = require('http');
+//1. URL 모듈을 가져온다
+var url = require('url');
+
+http.createServer(function handler(req, res) {
+	// 클라이언트가 보낸 name 파라미터 값을 변수에 저장하라!
+	//2. URL 정보를 분석한다.
+	var urlMap = url.parse(req.url, true);
+	
+	//3. 쿼리 객체에서 파라미터 값을 추출한다.
+	var v1 = parseInt(urlMap.query.v1, 10);
+	var v2 = parseInt(urlMap.query.v2, 10);
+	var op = urlMap.query.op;
+	
+    res.writeHead(200, 'ok', 
+    		{'Content-Type': 'text/html;charset=UTF-8'});
+    res.write('<html><head><title>test04</title></head>');
+    res.write('<body>');
+   
+    switch(op){
+    case 'plus':
+    	res.write(v1 + ' + ' + v2 + ' = ' + (v1 + v2));
+    	break;
+    case 'minus':
+    	res.write(v1 + ' - ' + v2 + ' = ' + (v1 - v2));
+    	break;
+    case 'multiple':
+    	res.write(v1 + ' * ' + v2 + ' = ' + (v1 * v2));
+    	break;
+    case 'divide':
+    	res.write(v1 + ' / ' + v2 + ' = ' + (v1 / v2));
+    	break;
+    default:
+    	res.write('해당 연산자가 없습니다!');
+    }
+    res.write('</body></html>');
+    res.end();
+	console.log('okoko');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
