7b4d351cf9fa88ef79cb5fb263ec7080b9fd7466
jinyoung <jinyoung.eom@gmail.com>
2014-10-13 12:51:34 +0900

    .

---
+++ java01/src/net/java63/test/HelloWorld.java
@@ -0,0 +1,7 @@
+package net.java63.test;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.print("나의 첫 번째 자바 프로그램!\n");
+  }
+}
---
+++ java01/src/net/java63/test2/Ohora.java
@@ -0,0 +1,10 @@
+package net.java63.test2;
+
+public class Ohora {
+  public static void main(String[] args) {
+    int v1 = 1;
+    int v2 = 3;
+    
+    System.out.print(v1 + " + " + v2 + " = " + (v1 + v2));
+  }
+}
