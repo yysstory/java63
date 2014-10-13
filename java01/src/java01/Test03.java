23063c1bed06ae094ffd5428ff3a2824af0a76cd
jinyoung <jinyoung.eom@gmail.com>
2014-10-13 12:25:39 +0900

    .

---
+++ java01/src/java01/Test03.java
@@ -0,0 +1,45 @@
+/* main(): JVM이 클래스를 실행할 때 호출하는 함수.
+ * - start entry
+ * - 다음 소스의 규칙에 따라 작성하라!
+ * 
+ * 자바 클래스 실행
+ * - java -classpath [클래스 파일이 있는 경로] [클래스명]
+ * 
+ * 만약 클래스가 특정 패키지에 소속된 멤버라면,
+ * - java -classpath [패키지가 있는 경로] [패키지명.클래스명]
+ * 
+ * -classpath 대신 -cp 라고 해도 됨.
+ * 
+ * 패키지명.클래스명 = Fully Qualified Name(QName)
+ */
+package java01;
+
+public class Test03 {
+  public static void main(String[] args) {
+    // 콘솔 창으로 출력
+    System.out.print("Hello, World\n");
+  }
+}
+
+
+/* Quiz:
+ * 1) net.java63.test 패키지를 생성하라!
+ * 2) 이 패키지에 HelloWorld 이름을 갖는 클래스를 생성하라!
+ * 3) 클래스를 실행하면 다음과 같이 출력되게 작성하라!
+ *    > 나의 첫 번째 자바 프로그램!
+ * 4) 터미널을 사용하여 HelloWorld 클래스를 실행하라!
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
