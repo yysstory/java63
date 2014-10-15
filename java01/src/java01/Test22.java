8409e8920a9bb3590182d83beebf6372e4b18116
jinyoung <jinyoung.eom@gmail.com>
2014-10-15 11:35:11 +0900

    .

---
+++ java01/src/java01/Test22.java
@@ -0,0 +1,35 @@
+/* 반복문
+ - while (조건) 명령문;
+ - while (조건) { ... }
+
+ - do 명령문; while(조건);
+ - do { ... } while(조건);
+
+  - for (초기화명령문; 조건; 증가문) { ... }
+  - for (변수 : 배열이나 Collection 타입 객체) { ... }
+ */
+package java01;
+
+public class Test22 {
+
+  public static void main(String[] args) {
+    // 퀴즈
+    // - 1에서 100까지 출력하라. 단, 4의 배수를 출력하라!
+    // - 단 20의 배수이고 60의 배수가 아닌 것은 출력하지 말아라.
+   // - do 문을 사용하라
+    int x = 1;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
