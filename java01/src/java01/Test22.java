60d156f30bbe5b1f2a5546d1df3b2ff35575bc47
jinyoung <jinyoung.eom@gmail.com>
2014-10-15 12:06:49 +0900

    .

--- java01/src/java01/Test22.java
+++ java01/src/java01/Test22.java
@@ -15,9 +15,35 @@ public class Test22 {
   public static void main(String[] args) {
     // 퀴즈
     // - 1에서 100까지 출력하라. 단, 4의 배수를 출력하라!
-    // - 단 20의 배수이고 60의 배수가 아닌 것은 출력하지 말아라.
-   // - do 문을 사용하라
+    //   다음과 같이 출력하라!
+    //   4, .....20
+    //   24, 28, ...
+    // - 단 20의 배수일 때 마다 다음 라인으로 가라!
+    //   20의 배수이지만 60의 배수인 경우는 새 라인 앞에만 @문자를 붙여라
+    //
+    //   4, 8, 12, 16, 20(엔터)
+    //   24, 28, 32, 36, 40(엔터)
+    //   44, 48, 52, 56, 60(엔터) 
+    //   @64, 68, 72, 76, 80(엔터)
+    //   84, 88, 92, 96, 100
+    //   
+   // - do ~ while 문을 사용하라
+    long startMilliSec = System.currentTimeMillis();
     int x = 1;
+    do {
+      if(x%4 == 0){
+        System.out.print(x+" ");        
+        if(x%20 == 0){
+          System.out.println();  
+          if(x%60 == 0){
+            System.out.print("@");        
+          }
+        }
+      }
+      x++;
+    } while (x <= 210000);
+    long endMillisSec = System.currentTimeMillis();
+    System.out.println(endMillisSec - startMilliSec);
   }
 
 }
