d5fb0408fd53e332369d3d39b7600b765c1e9441
jinyoung <jinyoung.eom@gmail.com>
2014-10-20 14:46:40 +0900

    .

--- java01/src/java01/test41/Test41.java
+++ java01/src/java01/test41/Test41.java
@@ -4,7 +4,7 @@
  */
 package java01.test41;
 
-public class Test40 {
+public class Test41 {
   public static void main(String[] args) {
     Car c1 = new Car("현다이", "마구타", 1997);
     Car c2 = new Car("가이아", "M5", 2500);
