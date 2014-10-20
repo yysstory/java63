2118ad44f19f32fff2b870ee49b44d3a2c9d2aa8
jinyoung <jinyoung.eom@gmail.com>
2014-10-20 12:58:34 +0900

    .

---
+++ java01/src/java01/test41/Car.java
@@ -0,0 +1,24 @@
+package java01.test41;
+
+public class Car {
+  String maker;
+  String model;
+  int cc = 800; 
+  boolean diesel;
+  java.util.Date releaseDate;
+  int capacity = 5; // 초기화 문장 
+  
+  public Car(String maker, String model, int cc) {
+    this.maker = maker;
+    this.model = model;
+    this.cc = cc;
+  }
+}
+
+
+
+
+
+
+
+
