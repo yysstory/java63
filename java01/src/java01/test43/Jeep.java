937e65c5452779cf56526da808ff590b1549a650
jinyoung <jinyoung.eom@gmail.com>
2014-10-20 14:45:24 +0900

    .

---
+++ java01/src/java01/test43/Jeep.java
@@ -0,0 +1,13 @@
+package java01.test43;
+
+public class Jeep extends Car {
+  /* 서브 클래스들의 공통속성/연산자를 수퍼 클래스로 옮기고, 
+   * 상속 받는다.
+  String maker;
+  String model;
+  int cc;
+  int capacity;
+  java.util.Date releaseDate;
+  */
+  int weight;
+}
---
+++ java01/src/java01/test43/Tico.java
@@ -0,0 +1,13 @@
+package java01.test43;
+
+public class Tico extends Car{
+  /* 서브 클래스들의 공통 속성/연산자(메서드)를 수퍼 클래스로 옮긴다.
+   * 그리고, 상속 받는다.
+  String maker;
+  String model;
+  int cc;
+  int capacity;
+  java.util.Date releaseDate;
+  */
+  boolean sunRoof;
+}
