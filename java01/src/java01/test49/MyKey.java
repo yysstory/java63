a34edaff53a4335f02ad629c5d6bac31855be5e0
jinyoung <jinyoung.eom@gmail.com>
2014-10-21 13:11:00 +0900

    .

---
+++ java01/src/java01/test49/MyKey.java
@@ -0,0 +1,70 @@
+/* Object 클래스
+ - hashCode(): 인스턴스가 별로 고유 값을 리턴
+ - equals(): 같은 인스턴스인지 비교(true/false)
+ 
+ Map에 값을 저장할 때 키로 사용할 클래스라면,
+ 반드시 그 클래스는 hashCode()와 equals()를 재정의 해야 한다.
+ 
+ String + 랩퍼 클래스(Byte, Short, Integer, Long, Float, Double, ..)
+ => 기본적으로 hashCode()와 equals()와 toString() 등을 재정의하였다.
+ => hashCode(); 인스턴스에 저장된 값이 같다면 같은 해시 값을 리턴한다.
+ => equals(): 인스턴스에 저장된 값이 같다면 true 리턴
+ => toString(): 인스턴스의 내용을 출력하도록 재정의함.
+                  즉, "클래스@해시" 문자열을 출력하지 않음.
+ */
+package java01.test49;
+
+public class MyKey {
+  String password;
+  int specialNo;
+  int secreteNo;
+  
+  public MyKey(String password, int specialNo, int secreteNo) {
+    this.password = password;
+    this.specialNo = specialNo;
+    this.secreteNo = secreteNo;
+  }
+  
+  @Override
+  public String toString() {
+    return "MyKey [password=" + password + 
+        ", specialNo=" + specialNo
+        + ", secreteNo=" + secreteNo + "]";
+  }
+  
+  // password, specialNo, secreteNo 값이 모두 같을 때는
+  // 같은 해시 값을 리턴하도록 hashCode()를 재정의한다.
+  @Override
+  public int hashCode() {
+    String temp = this.toString();
+    return temp.hashCode();
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) return false;
+    
+    if (!(obj instanceof MyKey)) return false;
+    
+    MyKey other = (MyKey) obj;
+    if (this.password.equals(other.password) &&
+        this.secreteNo == other.secreteNo &&
+        this.specialNo == other.specialNo) {
+      return true;
+    }
+    return false;
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
