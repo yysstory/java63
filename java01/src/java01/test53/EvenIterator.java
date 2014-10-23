aefbfb0c4cd606afa9a0ea89f8236a0c8896753c
jinyoung <jinyoung.eom@gmail.com>
2014-10-23 17:08:50 +0900

    .

---
+++ java01/src/java01/test53/EvenIterator.java
@@ -0,0 +1,32 @@
+package java01.test53;
+
+import java01.test53.step05.Iterator;
+
+public class EvenIterator extends Iterator {
+  int cursor;
+  
+  public EvenIterator() {}
+  
+  public boolean hasNext() {
+    if (cursor < list.length)
+      return true;
+    else 
+      return false;
+  }
+  
+  public String next() {
+    String value = list[cursor];
+    cursor += 2;
+    return value;
+  }
+}
+
+
+
+
+
+
+
+
+
+
