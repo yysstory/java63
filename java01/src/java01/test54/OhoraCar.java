d014a69e56648d3f4f0cfb2d6f137839f61745c3
jinyoung <jinyoung.eom@gmail.com>
2014-10-24 15:41:22 +0900

    .

---
+++ java01/src/java01/test54/OhoraCar.java
@@ -0,0 +1,17 @@
+package java01.test54;
+
+public class OhoraCar implements Unit {
+
+  @Override
+  public void run() {
+    System.out.println("빠~~~~~아~~앙....");
+    
+  }
+
+  @Override
+  public void stop() {
+    System.out.println("끼이익~~~~==========|");
+    
+  }
+
+}
