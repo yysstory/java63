7509a1db52ec2f901620cd05f3fa733e665aecfe
jinyoung <jinyoung.eom@gmail.com>
2014-10-27 12:57:12 +0900

    .

--- java02/src/java02/test01/Exception07.java
+++ java02/src/java02/test01/Exception07.java
@@ -1,51 +1,48 @@
-/* Exception06의 고통을 해결함.
- 
+/* RuntimeException 계열을 사용하여 Exception06의 고통을 해결함.
       
  */
 package java02.test01;
 
 import java.util.Scanner;
 
-// RuntimeException을 사용하지 않을 때! 고통!
-// => 상위 호출자에게 오류를 던지기 위해 
-//    메서드 선언에 반드시 throws 명령을 붙여야 한다.
-// 예) printCompute(), printContent(), printPage()를 보라!
 public class Exception07 {
   static int a;
   static int b;
   static String op;
   
   static class Calculator {
-    // Exception 계열의 예외를 던지는 메서드는 반드시 
-    // 메서드 선언부에 throws Exception 처럼 지정해야 한다.
+    // Exception 대신 RuntimeException을 던진다.
+    // => Error처럼 메서드 선언부에 지정하지 않아도 된다.
     public static int compute(int a, int b, String op) 
-        throws Exception { 
+        /*throws Exception*/ { 
       switch (op) {
       case "+": return a + b;
       case "-": return a - b;
       default: 
-        throw new Exception("지원하지 않는 연산자입니다.");
+        throw new RuntimeException("지원하지 않는 연산자입니다.");
       }
     }
   }
   
-  // compute()에서 발생하는 예외를 처리하기 싫으면 상위 호출자에게 넘긴다.
-  // 문법: 함수 선언부에 "throws 예외명" 추가
-  public static void printResult() throws Exception {
+  // compute()에서는 RuntimeException을 던진다.
+  // try... catch...로 처리하지 않으면,
+  // *****자동*****으로 상위 호출자에게 던진다.
+  // throws 절을 선언하지 않아도 된다.
+  public static void printResult() /*throws Exception*/ {
     System.out.println("a = " + a);
     System.out.println("b = " + b);
     System.out.println("결과는 = " + Calculator.compute(a, b, op));
   }
   
-  // printResult()에서 던진 예외를 처리하기 싫으면 상위 호출자에게 넘긴다.
-  public static void printContent() throws Exception {
+  public static void printContent() /*throws Exception*/ {
     System.out.println("******************");
     printResult();
     System.out.println("******************");
   }
   
-  // printContent()에서 던진 예외를 처리하기 싫으면 상위 호출자에게 넘긴다.
-  public static void printPage() throws Exception {
+  // 이렇게 일반 메서드 호출하듯이 printContent()를 호출할 수 있다.
+  // throws절 생략가능! => 코드가 간결해짐.
+  public static void printPage() /*throws Exception*/ {
     System.out.println("머리말 정보....");
     printContent();
     System.out.println("꼬리말 정보....");
@@ -62,8 +59,9 @@ public class Exception07 {
     System.out.print("연산자=");
     op = scanner.nextLine();
     
-    //compute()에서 발생된 예외를 printResult()가 아닌
-    // main()에서 처리한다.
+    // compute()에서 발생된 예외를 중간의 메서드에서는 처리할 필요가 없다.
+    // 이렇게 처음 호출자가 통합하여 처리하며 됨.
+    // 오류관리가 쉽고, 코드가 간결해짐.
     try {
       printPage();
     } catch (Exception ex) {
