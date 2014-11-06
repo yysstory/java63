c21257c06250c31ef11233b0c179678eb3eb55c1
jinyoung <jinyoung.eom@gmail.com>
2014-11-06 10:35:17 +0900

    .

---
+++ java02/src/java02/test13/MyClass.java
@@ -0,0 +1,9 @@
+package java02.test13;
+
+public class MyClass {
+  int i;
+  
+  public void m() {
+    System.out.println("i = " + i);
+  }
+}
---
+++ java02/src/java02/test13/Test02.java
@@ -0,0 +1,10 @@
+package java02.test13;
+
+public class Test02 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
