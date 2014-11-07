54965c29c1798b8f9bed16ec6a2acdac0de69a51
jinyoung <jinyoung.eom@gmail.com>
2014-11-07 16:04:46 +0900

    .

---
+++ dbmodel/order-data.sql
@@ -0,0 +1,8 @@
+/* INSERT 문
+ * INSERT INTO 테이블명 (컬럼명, 컬럼명, ...)
+ * VALUES (값, 값, ...)
+ * 
+ */
+insert into ADDRS(postno, bas_addr) values(100-100, '서울 강남구');
+insert into ADDRS(postno, bas_addr) values(100-101, '서울 서초구');
+insert into ADDRS(postno, bas_addr) values(100-102, '서울 마포구');
