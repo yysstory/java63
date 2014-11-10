a36f6cdfa1680ead06f9fd1d25d7dc39cf38a1d1
jinyoung <jinyoung.eom@gmail.com>
2014-11-10 17:14:16 +0900

    .

--- java02/src/java02/Selector.java
+++ java02/src/java02/Selector.java
@@ -7,11 +7,11 @@ public class Selector {
 
   public static void main(String[] args) throws Exception {
     String[] students = new String[]{
-        "@권영근", "배연정", "송민준", "@지용근", "정해창", "@장대혁",
-        "@조민석", "김지영", "박기남", "이용호", "@염정우", "김종서",
+        "@권영근", "배연정", "@송민준", "@지용근", "정해창", "@장대혁",
+        "조민석", "@김지영", "박기남", "이용호", "@염정우", "김종서",
         "@김해경", "@한진수", "양기욱", "@이주형", "@허정아", "김광철",
         "반기백", "@윤용식", "@김원기", "나정담", "@현나래", "김기현",
-        "@조현권", "강다현", "김다은", "신경민", "@윤종익", "@정세라"
+        "조현권", "강다현", "김다은", "신경민", "@윤종익", "@정세라"
     };
     
     int no = -1;
@@ -41,16 +41,14 @@ public class Selector {
     Thread.sleep(800);
     
     if (list.size() == 1) {
-      System.out.print("^^ 당첨 => ");
-      Thread.sleep(3000);
       System.out.println(list.get(0));
       return;
     }
     
+    
     String name = list.remove((int)(Math.random() * list.size())); 
     
     if (!name.startsWith("@")) {
-      System.out.println("\n------------------");                                                                                                                                           
       System.out.println(name);
     }
     ping(list);
