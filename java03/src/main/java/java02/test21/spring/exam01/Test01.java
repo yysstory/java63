88a1ce4152887b3faa26699941287ec9ca7f55ff
jinyoung <jinyoung.eom@gmail.com>
2014-11-14 16:26:36 +0900

    .

--- java03/src/main/java/java02/test21/spring/exam01/Test01.java
+++ java03/src/main/java/java02/test21/spring/exam01/Test01.java
@@ -1,10 +1,33 @@
 package java02.test21.spring.exam01;
 
+/* Spring Bean Container 사용하기
+ * 1) ClassPathXmlApplicationContext => 클래스 경로에서 설정 파일을 찾는다.
+ * 2) FileSystemXmlApplicationContext => 설정 파일이 있는 경로 지정
+ * 
+ * 
+ */
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test01 {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    //Car c = new Car();
+    ApplicationContext ctx =
+        new ClassPathXmlApplicationContext(
+            new String[]{"java02/test21/spring/exam01/application-context.xml"});
+    
+    
   }
 
 }
+
+
+
+
+
+
+
+
+
