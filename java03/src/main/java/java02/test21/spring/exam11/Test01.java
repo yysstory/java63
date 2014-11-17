5e381abaf40ec04f075c288d8d7c6cbbc021d015
jinyoung <jinyoung.eom@gmail.com>
2014-11-17 12:58:48 +0900

    .

---
+++ java03/src/main/java/java02/test21/spring/exam11/Test01.java
@@ -0,0 +1,46 @@
+package java02.test21.spring.exam11;
+
+/* 스프링 설정
+ * => 호출할 생성자 지정하기
+ * 
+ */
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test01 {
+
+  @SuppressWarnings({ "unused", "resource" })
+  public static void main(String[] args) {
+    //Car c = new Car();
+    ApplicationContext ctx =
+        new ClassPathXmlApplicationContext(
+            new String[]{"java02/test21/spring/exam11/application-context.xml"});
+    
+    Engine e01 = (Engine)ctx.getBean("engine");
+    Engine e02 = (Engine)ctx.getBean("engine");
+    
+    if (e01 != e02) System.out.println("e01 != e02");
+    
+    
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
