a04faa101a7b4959bebef8f34e7c4a6121b13e4a
jinyoung <jinyoung.eom@gmail.com>
2014-11-17 11:30:53 +0900

    .

---
+++ java03/src/main/java/java02/test21/spring/exam07/Car.java
@@ -0,0 +1,54 @@
+package java02.test21.spring.exam07;
+
+import java.util.Arrays;
+
+public class Car {
+  String model;
+  int cc;
+  Engine engine;
+  Tire[] tires;
+  
+  public Car() {}
+  
+  public Car(String model) {
+    this.model = model;
+  }
+  
+  public Car(String model, int cc) {
+    this.model = model;
+    this.cc = cc;
+  }
+  
+  @Override
+  public String toString() {
+    return "Car [model=" + model + ", cc=" + cc + ", engine=" + engine
+        + ", tires=" + Arrays.toString(tires) + "]";
+  }
+
+  public Tire[] getTires() {
+    return tires;
+  }
+  public void setTires(Tire[] tires) {
+    this.tires = tires;
+  }
+  public String getModel() {
+    return model;
+  }
+  public void setModel(String model) {
+    this.model = model;
+  }
+  public int getCc() {
+    return cc;
+  }
+  public void setCc(int cc) {
+    this.cc = cc;
+  }
+  public Engine getEngine() {
+    return engine;
+  }
+  public void setEngine(Engine engine) {
+    this.engine = engine;
+  }
+  
+  
+}
---
+++ java03/src/main/java/java02/test21/spring/exam07/Test01.java
@@ -0,0 +1,47 @@
+package java02.test21.spring.exam07;
+
+/* 스프링 설정
+ * => 호출할 생성자 지정하기
+ * 
+ */
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test01 {
+
+  @SuppressWarnings({ "unused", "resource" })
+  public static void main(String[] args) {
+    //Car c = new Car();
+    ApplicationContext ctx =
+        new ClassPathXmlApplicationContext(
+            new String[]{"java02/test21/spring/exam07/application-context.xml"});
+    
+    Car c1 = (Car)ctx.getBean("b01");
+    System.out.println(c1);
+    
+    Car c2 = (Car)ctx.getBean("b02");
+    System.out.println(c2);
+    
+    
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
