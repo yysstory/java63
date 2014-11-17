dc800c26c620e52d52b4e8c7d589711f7bb50eff
jinyoung <jinyoung.eom@gmail.com>
2014-11-17 11:38:39 +0900

    .

---
+++ java03/src/main/java/java02/test21/spring/exam08/Car.java
@@ -0,0 +1,54 @@
+package java02.test21.spring.exam08;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Car {
+  String model;
+  int cc;
+  Engine engine;
+  List<Tire> tires;
+  
+  public Car() {}
+  
+  public Car(String model) {
+    this.model = model;
+  }
+  
+  public Car(String model, int cc) {
+    this.model = model;
+    this.cc = cc;
+  }
+  
+  @Override
+  public String toString() {
+    return "Car [model=" + model + ", cc=" + cc + ", engine=" + engine
+        + ", tires=" + tires + "]";
+  }
+  public List<Tire> getTires() {
+    return tires;
+  }
+  public void setTires(List<Tire> tires) {
+    this.tires = tires;
+  }
+  public String getModel() {
+    return model;
+  }
+  public void setModel(String model) {
+    this.model = model;
+  }
+  public int getCc() {
+    return cc;
+  }
+  public void setCc(int cc) {
+    this.cc = cc;
+  }
+  public Engine getEngine() {
+    return engine;
+  }
+  public void setEngine(Engine engine) {
+    this.engine = engine;
+  }
+  
+  
+}
