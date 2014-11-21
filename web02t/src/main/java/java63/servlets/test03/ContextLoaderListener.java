cbcea9f280bb8443edaf53baf8b1d29c867a34b9
jinyoung <jinyoung.eom@gmail.com>
2014-11-21 11:55:05 +0900

    .

---
+++ web02t/src/main/java/java63/servlets/test03/ContextLoaderListener.java
@@ -0,0 +1,20 @@
+package java63.servlets.test03;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextLoaderListener implements ServletContextListener {
+
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+    
+
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
+    
+
+  }
+
+}
