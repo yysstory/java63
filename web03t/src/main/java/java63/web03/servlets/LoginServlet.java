bd7a996d3003edc7530d47036c1d1acddb5b77d3
jinyoung <jinyoung.eom@gmail.com>
2014-11-27 16:28:50 +0900

    .

--- web03t/src/main/java/java63/web03/servlets/LoginServlet.java
+++ web03t/src/main/java/java63/web03/servlets/LoginServlet.java
@@ -1,6 +1,9 @@
 package java63.web03.servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java63.web03.dao.MemberDao;
+import java63.web03.domain.Member;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +12,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 
 @WebServlet("/auth/login.do")
@@ -54,12 +61,25 @@ public class LoginServlet extends HttpServlet {
         cookie.setMaxAge(0); // 무효화시킴
         response.addCookie(cookie);
       }
-      /*
+      
       ApplicationContext appCtx =
           WebApplicationContextUtils.getWebApplicationContext(
               this.getServletContext());
-      */
+      MemberDao memberDao = (MemberDao)appCtx.getBean("memberDao");
+      HashMap<String,String> params = new HashMap<>();
+      params.put("userId", uid);
+      params.put("password", pwd);
+      Member member = memberDao.existUser(params);
       
+      HttpSession session = request.getSession();
+
+      if (member != null) {
+        session.setAttribute("loginUser", member);
+        response.sendRedirect("../product/list.do");
+      } else {
+        session.invalidate(); // 세션을 제거하고 새로 만든다.
+        response.sendRedirect("login.do"); //로그인 폼으로 보낸다.
+      }
 
     } catch (Exception e) {
       RequestDispatcher rd = 
