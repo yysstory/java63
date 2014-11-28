7915c13904eaf8c17c9141f19629fc00d367e444
jinyoung <jinyoung.eom@gmail.com>
2014-11-28 12:21:14 +0900

    .

---
+++ web03t/src03/main/java/java63/web03/control/LogoutServlet.java
@@ -0,0 +1,26 @@
+package java63.web03.control;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+
+@Component("/auth/logout.do")
+public class LogoutServlet {
+  public String execute(HttpServletRequest request) throws Exception {
+    request.getSession().invalidate();
+    return "redirect:login.do";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
