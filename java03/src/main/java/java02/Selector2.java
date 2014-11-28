52c4e92d9eaf3282cf9b5449f77ebeea0bbb3acd
jinyoung <jinyoung.eom@gmail.com>
2014-11-28 18:36:01 +0900

    z

--- java03/src/main/java/java02/Selector2.java
+++ java03/src/main/java/java02/Selector2.java
@@ -7,11 +7,11 @@ public class Selector2 {
 
   public static void main(String[] args) throws Exception {
     String[] students = new String[]{
-        "@권영근", "배연정", "@송민준", "@지용근", "정해창", "@장대혁",
-        "@조민석", "@김지영", "@박기남", "이용호", "@염정우", "김종서",
-        "@김해경", "@한진수", "양기욱", "@이주형", "@허정아", "김광철",
+        "@권영근", "배연정", "송민준", "지용근", "정해창", "장대혁",
+        "조민석", "김지영", "박기남", "이용호", "@염정우", "김종서",
+        "@김해경", "@한진수", "양기욱", "@이주형", "허정아", "김광철",
         "반기백", "@윤용식", "김원기", "나정담", "@현나래", "김기현",
-        "조현권", "@강다현", "김다은", "신경민", "윤종익", "@정세라"
+        "@조현권", "@강다현", "김다은", "신경민", "@윤종익", "정세라"
     };
     
     int no = -1;
@@ -19,24 +19,25 @@ public class Selector2 {
     ArrayList<String> selected = new ArrayList<String>();
     
     for (int i = 0; i < students.length; i++) {
-      //no = (int)(Math.random() * 30);
+      no = (int)(Math.random() * 30);
       selected.add(students[i]);
     }
     
     ping(selected);
+    //System.out.println(students[(int)(Math.random() * 30)]);
     
   }
   
   public static void ping(ArrayList<String> list) throws Exception {
     
     int count = 1;
-    /*
+    
     for (String name : list) {
       System.out.print(name + ",");
       if ((count % 5) == 0)
         System.out.println();
       count++;
-    }*/
+    }
     
     Thread.sleep(100);
     
@@ -49,7 +50,7 @@ public class Selector2 {
     
     String name = list.remove((int)(Math.random() * list.size())); 
     
-    if (!name.startsWith("@")) {
+    if (name.startsWith("@")) {
       //System.out.println("\n------------------");                                                                                                                                           
       System.out.println(name);
     }
