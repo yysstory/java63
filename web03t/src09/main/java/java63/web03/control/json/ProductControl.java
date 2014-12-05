11e2793d5244029ca85a3cde38cd808aee2adee3
jinyoung <jinyoung.eom@gmail.com>
2014-12-05 09:49:39 +0900

    .

---
+++ web03t/src09/main/java/java63/web03/control/json/ProductControl.java
@@ -0,0 +1,124 @@
+package java63.web03.control.json;
+
+import java.io.File;
+import java.util.HashMap;
+import java63.web03.dao.MakerDao;
+import java63.web03.dao.ProductDao;
+import java63.web03.domain.Product;
+
+import javax.servlet.ServletContext;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller("json.productControl")
+@RequestMapping("/json/product")
+public class ProductControl {
+  static Logger log = Logger.getLogger(ProductControl.class);
+  static final int PAGE_DEFAULT_SIZE = 5;
+  
+  @Autowired MakerDao makerDao;
+  @Autowired ProductDao productDao;
+  @Autowired ServletContext servletContext;
+ 
+  @RequestMapping(value="/add", method=RequestMethod.POST)
+  public Object add(Product product) throws Exception {  
+    
+    productDao.insert(product);
+    
+    if (product.getPhotofile() != null
+        && !product.getPhotofile().isEmpty()) {
+
+      String fileuploadRealPath = 
+        servletContext.getRealPath("/fileupload");
+      String filename = System.currentTimeMillis() + "_"; 
+      File file = new File(fileuploadRealPath + "/" + filename);
+    
+      product.getPhotofile().transferTo(file);
+      product.setPhoto(filename);
+        
+      productDao.insertPhoto(product);
+    }
+    
+    HashMap<String,Object> resultMap = new HashMap<>();
+    resultMap.put("status", "success");
+    
+    return resultMap;
+  }
+
+  @RequestMapping("/delete")
+  public Object delete(int no) throws Exception {
+    productDao.deletePhoto(no);
+    productDao.delete(no);
+    
+    HashMap<String,Object> resultMap = new HashMap<>();
+    resultMap.put("status", "success");
+    
+    return resultMap;
+  }
+  
+  @RequestMapping("/list")
+  public Object list(
+      @RequestParam(defaultValue="1") int pageNo,
+      @RequestParam(defaultValue="5") int pageSize) throws Exception {
+    
+    if (pageSize <= 0)
+      pageSize = PAGE_DEFAULT_SIZE;
+    
+    int totalSize = productDao.totalSize();
+    int maxPageNo = totalSize / pageSize;
+    if ((totalSize % pageSize) > 0) maxPageNo++;
+    
+    if (pageNo <= 0) pageNo = 1;
+    if (pageNo > maxPageNo) pageNo = maxPageNo;
+    
+    HashMap<String,Object> paramMap = new HashMap<>();
+    paramMap.put("startIndex", ((pageNo - 1) * pageSize));
+    paramMap.put("pageSize", pageSize);
+    
+    HashMap<String,Object> resultMap = new HashMap<>();
+    resultMap.put("status", "success");
+    resultMap.put("currPageNo", pageNo);
+    resultMap.put("maxPageNo", maxPageNo);
+    resultMap.put("products", productDao.selectList(paramMap));
+    
+    return resultMap;
+  }
+  
+  @RequestMapping("/update")
+  public Object update(Product product) throws Exception {
+    productDao.update(product);
+    
+    HashMap<String,Object> resultMap = new HashMap<>();
+    resultMap.put("status", "success");
+    return resultMap;
+  }
+  
+  @RequestMapping("/view")
+  public Object view(int no, Model model) throws Exception {
+    Product product = productDao.selectOne(no);
+    
+    HashMap<String,Object> resultMap = new HashMap<>();
+    resultMap.put("status", "success");
+    resultMap.put("product", product);
+    resultMap.put("photos", productDao.selectPhoto(product.getNo()));
+    return resultMap;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ web03t/src09/main/webapp/product/app.js
@@ -0,0 +1,96 @@
+var currPageNo;
+var maxPageNo;
+
+//$(document).ready(function(){});
+$(function(){
+  $('.header').load('../common/header.html');
+  $('.footer').load('../common/footer.html');
+  $('.form').load('form.html');
+  
+  loadProductList(1);
+  
+  $(document).on('click', '.data-row a', function(){
+    loadProduct($(this).attr('data-no'));
+  });
+});
+
+
+
+$('#prevBtn').click(function(event){
+	if (currPageNo > 1) {
+	  loadProductList(currPageNo - 1);
+	}
+});
+
+$('#nextBtn').click(function(event){
+	if (currPageNo < maxPageNo) {
+	  loadProductList(currPageNo + 1);
+	}
+});
+
+function setPageNo(currPageNo, maxPageNo) {
+  window.currPageNo = currPageNo;
+  window.maxPageNo = maxPageNo;
+  
+  $('#pageNo').html(currPageNo);
+  
+  if (currPageNo <= 1) $('#prevBtn').css('display', 'none');
+  else $('#prevBtn').css('display', '');
+  
+  if (currPageNo >= maxPageNo) $('#nextBtn').css('display', 'none');
+  else $('#nextBtn').css('display', '');
+}
+	
+function loadProductList(pageNo) {
+  if (pageNo <= 0) pageNo = currPageNo;
+  
+	$.getJSON('../json/product/list.do?pageNo=' + pageNo, 
+    function(data){
+      setPageNo(data.currPageNo, data.maxPageNo);
+      var products = data.products;
+      
+      $('.data-row').remove();
+      
+      for (var i = 0; i < products.length; i++) {
+        $('<tr>').addClass('data-row')
+            .append($('<td>').html(products[i].no))
+            .append(
+            		$('<td>').append(
+            				$('<a>').attr('href', '#')
+            				        .attr('data-no', products[i].no)
+            				        .html(products[i].name)
+            		)
+            )
+            .append($('<td>').html(products[i].quantity))
+            .append($('<td>').html(products[i].maker))
+            .appendTo('#productTable')
+      }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
